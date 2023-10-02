06f4bc2e7284afb7dacf4937ce541e0619afaacb
he8477 <Robert.Chen@hermes.com.tw>
2023-10-02 15:12:08 +0800

    add navigation lib

---
+++ navigation/src/lib/navigation.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'his-navigation',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      navigation works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NavigationComponent {
+
+}

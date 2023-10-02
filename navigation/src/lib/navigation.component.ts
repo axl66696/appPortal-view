f819e1a02a578f2155535b469382d423087b3266
he8477 <Robert.Chen@hermes.com.tw>
2023-10-02 17:12:07 +0800

    finish navigation

--- navigation/src/lib/navigation.component.ts
+++ navigation/src/lib/navigation.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ButtonModule } from 'primeng/button';
+import {AppStore} from '@his-viewmodel/app-portal/dist'
+import { NavigationService } from './navigation.service';
 @Component({
   selector: 'his-navigation',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <p>
-      navigation works!
-    </p>
-  `,
-  styles: [
-  ]
+  imports: [CommonModule,ButtonModule],
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+  providers: [NavigationService]
 })
 export class NavigationComponent {
 
+  appInfo = signal<AppStore>({} as AppStore)
+
+  navigationService = inject(NavigationService);
+  collapsedButtonIcon = computed(() => {
+    if (
+      !this.navigationService.isCollapsed() &&
+      this.navigationService.isShowBody()
+    ) {
+      return 'pi pi-angle-double-left';
+    }
+    return 'pi pi-angle-double-right';
+  });
+
+  /** 開合導覽列
+   * @memberof NavigationComponent
+   */
+      changeCollapsed(): void {
+        this.navigationService.onCollapsClick();
+      }
 }

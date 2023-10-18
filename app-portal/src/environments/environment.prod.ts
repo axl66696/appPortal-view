b325d7f447f0f48b60877c97257a5c506b119454
Tommy Huang <axl66696@gmail.com>
2023-10-18 15:09:33 +0800

    html調整、subjec與邏輯調整

--- app-portal/src/environments/environment.prod.ts
+++ app-portal/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   wsUrl: 'ws://localhost:8080'
+  // wsUrl: 'ws://10.251.42.49:8080'
 }
--- app-portal/src/environments/environment.ts
+++ app-portal/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  // wsUrl: 'ws://10.251.42.49:8080'
   wsUrl: 'ws://localhost:8080'
 }
